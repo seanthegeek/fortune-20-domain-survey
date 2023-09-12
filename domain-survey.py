9af6d5d6de9d34172e4603d0545f96768e35ccf4
Sean Whalen <seanthegeek@users.noreply.github.com>
2023-09-12 19:34:03 -0400

    Add stats code

--- domain-survey.py
+++ domain-survey.py
@@ -4,10 +4,21 @@ import datetime
 import os
 import subprocess
 import csv
+import json
+from collections import OrderedDict
+
+from checkdmarc import get_base_domain
 
 combined_result_rows = []
+combined_stats = OrderedDict(dnssec=OrderedDict(true=0, false=0),
+                             spf=OrderedDict(true=0, false=0),
+                             dmarc_policy=(OrderedDict(no_dmarc_record=0,
+                                                       none=0,
+                                                       quarantine=0,
+                                                       reject=0)),
+                                                       mx=OrderedDict(no_mx=0))
 
-date = datetime.datetime.today().strftime('%Y-%m-%d')
+date = datetime.datetime.utcnow().strftime('%Y-%m-%d')
 results_dir = os.path.join("results", date)
 os.makedirs(results_dir, exist_ok=True)
 domain_lists = [f for f in os.listdir("domains") if
@@ -29,13 +40,36 @@ for filename in sorted(domain_lists):
                     "-n", "1.1.1.1"])
     with (open(f"{output_filename}.csv")) as results_csv:
         reader = csv.DictReader(results_csv)
-        csv_fields = list(reader.fieldnames).insert("owner", 1)
+        csv_fields = list(reader.fieldnames).insert(1, "owner")
         for row in reader:
             row["owner"] = symbol
             combined_result_rows.append(row)
-combined_result_rows = sorted(combined_result_rows, key= lambda x: x["domain"])
-combined_filename = f"{date}_fortune-20-combined_checkdmarc.csv"
-with open(os.path.join(results_dir, combined_filename), "w") as combined_csv_file:
+            combined_stats["dnssec"][row["dnssec"].lower()] += 1
+            combined_stats["spf"][row["spf_valid"].lower()] += 1
+            if row["dmarc_p"] == "":
+                combined_stats["dmarc_policy"]["no_dmarc_record"] += 1
+            else:
+                combined_stats["dmarc_policy"][row["dmarc_p"]] += 1
+            if row["mx"] == "":
+                combined_stats["mx"]["no_mx"] += 1
+            else:
+                mx = get_base_domain(row["mx"].split("|")[0].split(" ")[-1])
+                if mx in combined_stats["mx"]:
+                    combined_stats["mx"][mx] += 1
+                else:
+                    combined_stats["mx"][mx] = 1
+combined_stats["spf"] = sorted(combined_stats["spf"],
+                               key=lambda x: x["spf"], reverse=True)
+combined_stats["dmarc_policy"] = sorted(combined_stats["dmarc_policy"],
+                               key=lambda x: x["dmarc_policy"], reverse=True)
+combined_stats["mx"] = sorted(combined_stats["mx"],
+                               key=lambda x: x["mx"], reverse=True)
+combined_result_rows = sorted(combined_result_rows, key=lambda x: x["domain"])
+combined_filename = f"{date}_fortune-20-combined_checkdmarc"
+combined_results_path = os.path.join(results_dir, combined_filename)
+with open(f"{combined_results_path}.csv", "w") as combined_csv_file:
     writer = csv.DictWriter(combined_csv_file, csv_fields)
     writer.writeheader()
     writer.writerows(combined_result_rows)
+with open(f"{combined_results_path}_stats.json", "w") as combined_stats_file:
+    combined_stats_file.write(json.dumps(combined_stats, indent=2))
